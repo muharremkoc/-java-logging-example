198facc306dd01c570855b35a8d1c309bf034478
muharremkoc <80245013+muharremkoc@users.noreply.github.com>
2021-05-24 09:52:17 +0300

    Log4j Updated

---
+++ src/main/java/com/maven/example/Person.java
@@ -0,0 +1,35 @@
+package com.maven.example;
+
+
+public class Person {
+    private String name;
+    private String surName;
+    private int age;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
